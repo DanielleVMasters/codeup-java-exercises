cb7c4bdbc8891f39a6d5019efbd90503f37d6328
DanielleVMasters <danielle.victoria.masters@gmail.com>
2023-01-02 11:57:33 -0600

    assignment complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,32 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 11;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Hiya!";
+        System.out.println(myString);
+
+        double myNumber;
+        myNumber = 3.14;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+        int a = 4;
+        a += 5;
+        System.out.println(a);
+
+        int b = 3;
+        int c = 4;
+        System.out.println(b * c);
+
+        int d = 10;
+        int e = 2;
+        System.out.println(d / e);
+        System.out.println(d - x);
     }
 }
