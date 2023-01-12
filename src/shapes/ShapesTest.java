a5a7afd1d14630da82dabf47f6562500184ad868
DanielleVMasters <danielle.victoria.masters@gmail.com>
2023-01-12 14:36:27 -0600

    Assignment complete

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,16 +2,28 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(5,4);
+        Measurable myShape = new Rectangle(4, 5);
 
-        System.out.println(box1.length);
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
+        System.out.println("myShape.getArea() = " + myShape.getArea());
 
-        System.out.println(box1.getArea());
-        System.out.println(box1.getPerimeter());
+//        myShape = new Square(5);
 
-        Rectangle box2 = new Square(5);
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
+        System.out.println("myShape.getArea() = " + myShape.getArea());
 
-        System.out.println(box2.getArea());
-        System.out.println(box2.getPerimeter());
     }
-}
+
+    }
+//        Rectangle box1 = new Rectangle(5,4);
+//
+//        System.out.println(box1.length);
+//
+//        System.out.println(box1.getArea());
+//        System.out.println(box1.getPerimeter());
+//
+//        Rectangle box2 = new Square(5);
+//
+//        System.out.println(box2.getArea());
+//        System.out.println(box2.getPerimeter());
+//    }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,19 +1,46 @@
 package shapes;
 
-public class Square extends Rectangle{
+public class Square extends Quadrilateral{
+        public Square(double side) {
+            super(side, side);
+        }
 
-    public Square(double side){
-        super(side, side);
-    }
+        //
+        public double getArea(){
+            System.out.println("Square Method used");
+            double side = this.width;
+            return side * side;
+        }
 
-    public double getArea(){
-        double side = this.width;
-        return side * side;
-    }
+        public double getPerimeter(){
+            System.out.println("Square Method used");
+            double side = this.width;
+            return side * 4;
+        }
 
-    public double getPerimeter(){
-        double side = this.width;
-        return side * 4;
-    }
+
+        @Override
+        public void setLength(double length) {
+            this.length = length;
+        }
+
+        @Override
+        public void setWidth(double width) {
+            this.width = width;
+        }
+
+    //    public Square(double side){
+//        super(side, side);
+//    }
+//
+//    public double getArea(){
+//        double side = this.width;
+//        return side * side;
+//    }
+//
+//    public double getPerimeter(){
+//        double side = this.width;
+//        return side * 4;
+//    }
 
 }
